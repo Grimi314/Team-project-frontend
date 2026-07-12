3047a3f7459048e5ad7a62db421ef8fb3eebabd6
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-07-13 01:06:08 +0200

    edited AddStory

--- src/app/components/AddStory/AddStory.tsx
+++ src/app/components/AddStory/AddStory.tsx
@@ -1,13 +1,15 @@
 import css from '@/app/components/AddStory/AddStory.module.css';
-
+import Header from '../header/header';
 import AddStoryForm from '../AddStoryForm/AddStoryForm';
-
+import Footer from '../footer/footer';
 export default function AddStory() {
   return (
     <main className={css.page}>
       <div className={css.container}>
+        <Header />
         <h1 className={css.title}>Створити нову історію</h1>
         <AddStoryForm />
+        <Footer />
       </div>
     </main>
   );
