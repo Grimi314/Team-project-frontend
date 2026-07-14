1ca93ff1430c853ac4bbfbffac9e601ed9fdd358
SashaOsp <sasha.mykol@gmail.com>
2026-07-14 15:01:47 +0300

    storyPage fix

--- src/app/components/storyPage/storyPage.tsx
+++ src/app/components/storyPage/storyPage.tsx
@@ -1,6 +1,9 @@
 import StoryDetails from '../storyDetails/storyDetails';
 import SaveStory from '../saveStory/saveStory';
-// import RecommendedStories from "../RecommendedStories/RecommendedStories";
+// import RecommendedStories from "../recommendedStories/recommendedStories";
+
+import { api } from '@/lib/api/axios';
+import { endpoints } from '@/lib/api/endpoints';
 
 import { Story } from '@/types/story';
 
@@ -8,15 +11,17 @@ type Props = {
   storyId: string;
 };
 
-export default function StoryPage({ storyId }: Props) {
-  const { data: story } = await api.get(endpoints.stories.byId(storyId));
+export default async function StoryPage({ storyId }: Props) {
+  const { data: story } = await api.get<Story>(endpoints.stories.byId(storyId));
 
   return (
     <>
       <StoryDetails story={story} />
-      <SaveStory storyId={story.id} />0
-      {/* TODO після merge з main перевірити наявність RecommendedStories та розкоментувати його, якщо він є */}
-      {/* <RecommendedStories /> */}
+
+      <SaveStory storyId={story.id} />
+
+      {/* TODO: Після merge з main перевірити наявність RecommendedStories та розкоментувати компонент. */}
+      {/* <recommendedStories /> */}
     </>
   );
 }
