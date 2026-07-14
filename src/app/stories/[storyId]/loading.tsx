c88f6e3aec0606661bfa0d0c7b743bd4c59826ef
Max <maxpuga87@gmail.com>
2026-07-14 18:17:31 +0200

    Add loading components

---
+++ src/app/stories/[storyId]/loading.tsx
@@ -0,0 +1,5 @@
+import { Loader } from '@/app/components/loader/loader';
+
+export default function StoryLoading() {
+  return <Loader label="Завантажуємо історію..." />;
+}
---
+++ src/app/stories/loading.tsx
@@ -0,0 +1,5 @@
+import { Loader } from '@/app/components/loader/loader';
+
+export default function StoriesLoading() {
+  return <Loader label="Завантажуємо статті..." />;
+}
